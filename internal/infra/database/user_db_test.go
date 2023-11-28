4cb2031d722208fb2dd8ffcb36a6dcbc0d7d0e58
victorbrugnolo <brugnolovictor@gmail.com>
2023-11-28 19:26:36 -0300

    add user find by email test

--- internal/infra/database/user_db_test.go
+++ internal/infra/database/user_db_test.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"github.com/victorbrugnolo/go-api-example/internal/entity"
 	"gorm.io/gorm"
@@ -31,6 +32,37 @@ func TestCreateUser(t *testing.T) {
 
 	var userFound entity.User
 	err = db.First(&userFound, "id = ?", user.ID).Error
+
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNil(t, userFound.Password)
+}
+
+func TestUser_FindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = db.AutoMigrate(&entity.User{})
+
+	if err != nil {
+		return
+	}
+
+	user, _ := entity.NewUser("John", "j@j.com", "123456")
+	userDB := NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail(user.Email)
+
+	fmt.Println(userFound)
+	fmt.Println(user)
 
 	assert.Nil(t, err)
 	assert.Equal(t, user.ID, userFound.ID)
